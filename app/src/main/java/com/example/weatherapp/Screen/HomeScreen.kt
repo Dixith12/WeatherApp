5abad786a39bb0a6b28c6faf65ddb02b3d346932
Deekshith <deekshithskulal485@gmail.com>
2025-07-23 22:30:47 +0530

    feat: added HomeScreen with content

--- app/src/main/java/com/example/weatherapp/Screen/HomeScreen.kt
+++ app/src/main/java/com/example/weatherapp/Screen/HomeScreen.kt
@@ -2,7 +2,6 @@ package com.example.weatherapp.Screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,11 +11,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
+
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
