1c4818a08bb156e40f1286789a88859a4633772a
Deekshith <deekshithskulal485@gmail.com>
2025-07-24 22:34:17 +0530

    feat: Add splash screen layout and navigation

--- app/src/main/java/com/example/weatherapp/screen/HomeScreen.kt
+++ app/src/main/java/com/example/weatherapp/screen/HomeScreen.kt
@@ -1,7 +1,8 @@
-package com.example.weatherapp.Screen
+package com.example.weatherapp.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,13 +12,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForwardIos
 
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -72,12 +78,15 @@ fun HomeScreenContent() {
                 fontSize = 25.sp,)
         }
         DetailCard()
+        Sevendays()
     }
 }
 
 @Composable
 fun DetailCard() {
-    Card(modifier = Modifier.padding(top = 20.dp))
+   Box(modifier = Modifier.padding(top = 20.dp)
+       .clip(RoundedCornerShape(25.dp))
+        .background(brush = Brush.linearGradient(listOf(Color(0xFF2F2383),Color(0xFF443A86),Color(0xFF443A86)))))
     {
         Row(modifier = Modifier.padding(vertical = 10.dp, horizontal = 23.dp),
             verticalAlignment = Alignment.CenterVertically)
@@ -87,13 +96,14 @@ fun DetailCard() {
                 Icon(painter = painterResource(id = R.drawable.wind),
                     contentDescription = "Wind",
                     modifier = Modifier.size(30.dp)
-                        .padding(bottom = 5.dp))
+                        .padding(bottom = 5.dp),
+                    tint = Color.White)
                 Text("13 km/h",
-                    color = Color.Black,
+                    color = Color.White,
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold)
                 Text("Wind",
-                    color = Color.Black,
+                    color = Color.White,
                     fontSize = 16.sp,)
 
             }
@@ -109,13 +119,14 @@ fun DetailCard() {
                 Icon(painter = painterResource(id = R.drawable.humidity),
                     contentDescription = "Wind",
                     modifier = Modifier.size(30.dp)
-                        .padding(bottom = 5.dp))
+                        .padding(bottom = 5.dp),
+                    tint = Color.White)
                 Text("13 km/h",
-                    color = Color.Black,
+                    color = Color.White,
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold)
                 Text("Humidity",
-                    color = Color.Black,
+                    color = Color.White,
                     fontSize = 16.sp,)
 
             }
@@ -131,16 +142,35 @@ fun DetailCard() {
                 Icon(painter = painterResource(id = R.drawable.material),
                     contentDescription = "Wind",
                     modifier = Modifier.size(30.dp)
-                        .padding(bottom = 5.dp))
+                        .padding(bottom = 5.dp),
+                    tint = Color.White)
                 Text("13 km/h",
-                    color = Color.Black,
+                    color = Color.White,
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold)
                 Text("Rain",
-                    color = Color.Black,
+                    color = Color.White,
                     fontSize = 16.sp,)
 
             }
         }
     }
+}
+
+@Composable
+fun Sevendays()
+{
+    Row(horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth().padding(vertical = 20.dp, horizontal = 15.dp))
+    {
+        Text("Today",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White)
+        Icon(imageVector = Icons.Default.ArrowForwardIos,
+            contentDescription = "ArrowBack",
+            tint=Color.White)
+    }
+
 }
