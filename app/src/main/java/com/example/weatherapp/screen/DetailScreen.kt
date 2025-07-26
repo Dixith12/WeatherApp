e84d32bd05b9051e8247c23bbd62c75de234fddc
Deekshith <deekshithskulal485@gmail.com>
2025-07-26 22:37:05 +0530

    feat:Added the Components to screen

--- app/src/main/java/com/example/weatherapp/screen/DetailScreen.kt
+++ app/src/main/java/com/example/weatherapp/screen/DetailScreen.kt
@@ -3,6 +3,7 @@ package com.example.weatherapp.screen
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -22,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,19 +48,28 @@ fun DetailScreen() {
 
 @Composable
 fun UpperSection() {
-    Row(modifier = Modifier.fillMaxWidth())
+    Row(modifier = Modifier.fillMaxWidth()
+        .padding(vertical = 10.dp, horizontal = 10.dp),
+        verticalAlignment = Alignment.CenterVertically)
     {
         Icon(imageVector = Icons.AutoMirrored.Default.ArrowBackIos,
                 contentDescription = "IconsBack",
                 modifier = Modifier.size(45.dp).border(width = 2.dp, color = Color.Gray, shape = CircleShape)
                     .padding(top = 10.dp, bottom = 10.dp, start = 13.dp, end = 8.dp),
                 tint = Color.White)
-        Icon(imageVector = Icons.Default.DateRange,
-            contentDescription = "DateRange",
-            tint = Color.White,
-            modifier = Modifier.size(45.dp))
+        Row(verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(start = 70.dp))
+        {
+            Icon(imageVector = Icons.Default.DateRange,
+                contentDescription = "DateRange",
+                tint = Color.White,
+                modifier = Modifier
+                    .padding(end = 5.dp)
+                    .size(42.dp))
             Text("7 days",
                 color = Color.White,
-                fontSize = 30.sp)
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold)
+        }
     }
 }
