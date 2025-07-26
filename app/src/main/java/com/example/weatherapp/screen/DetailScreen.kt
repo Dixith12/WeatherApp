970d151fb2e915e4ab49b9e74322418f47a049c1
Deekshith <deekshithskulal485@gmail.com>
2025-07-26 22:27:22 +0530

    feat: Enhancement of DetailScreen

--- app/src/main/java/com/example/weatherapp/screen/DetailScreen.kt
+++ app/src/main/java/com/example/weatherapp/screen/DetailScreen.kt
@@ -1,10 +1,64 @@
 package com.example.weatherapp.screen
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBackIos
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview
 @Composable
 fun DetailScreen() {
+    Box(modifier= Modifier.fillMaxSize()
+        .background(brush = Brush.linearGradient(listOf(
+            Color(0xFF2F2383), Color(0xFF443A86), Color(
+                0xFFBF4FD7
+            )
+        ))))
+    {
+        Column(modifier = Modifier.fillMaxSize())
+        {
+            UpperSection()
+        }
 
+    }
+}
+
+@Composable
+fun UpperSection() {
+    Row(modifier = Modifier.fillMaxWidth())
+    {
+        Icon(imageVector = Icons.AutoMirrored.Default.ArrowBackIos,
+                contentDescription = "IconsBack",
+                modifier = Modifier.size(45.dp).border(width = 2.dp, color = Color.Gray, shape = CircleShape)
+                    .padding(top = 10.dp, bottom = 10.dp, start = 13.dp, end = 8.dp),
+                tint = Color.White)
+        Icon(imageVector = Icons.Default.DateRange,
+            contentDescription = "DateRange",
+            tint = Color.White,
+            modifier = Modifier.size(45.dp))
+            Text("7 days",
+                color = Color.White,
+                fontSize = 30.sp)
+    }
 }
