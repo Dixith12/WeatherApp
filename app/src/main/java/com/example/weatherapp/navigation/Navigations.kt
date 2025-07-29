956e8ea6ad2f61e97eea38bcfd101db7f03c03d2
Deekshith <deekshithskulal485@gmail.com>
2025-07-29 10:56:11 +0530

    feat: Added Navigation

--- app/src/main/java/com/example/weatherapp/navigation/Navigations.kt
+++ app/src/main/java/com/example/weatherapp/navigation/Navigations.kt
@@ -1,1 +1,27 @@
 package com.example.weatherapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.weatherapp.screen.DetailScreen
+import com.example.weatherapp.screen.HomeScreen
+import com.example.weatherapp.screen.SplashScreen
+
+@Composable
+fun Navigation()
+{
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screens.SplashScreen.route)
+    {
+        composable(Screens.SplashScreen.route) {
+            SplashScreen()
+        }
+        composable(Screens.HomeScreen.route) {
+            HomeScreen()
+        }
+        composable(Screens.DetailScreen.route) {
+            DetailScreen()
+        }
+    }
+}
