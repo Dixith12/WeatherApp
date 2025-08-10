8e4e7916a599f322ad6c7f7c2b45759bc2040c45
Deekshith <deekshithskulal485@gmail.com>
2025-08-10 22:22:55 +0530

    feat: Added Room Database and Doa

--- app/src/main/java/com/example/weatherapp/room/WeatherDatabase.kt
+++ app/src/main/java/com/example/weatherapp/room/WeatherDatabase.kt
@@ -1,6 +1,13 @@
 package com.example.weatherapp.room
 
+import androidx.databinding.adapters.Converters
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.weatherapp.model.WeatherData
+import kotlin.uuid.Uuid
 
-
-abstract class WeatherDatabase {
+@Database(entities = [WeatherData::class],version = 1, exportSchema = false)
+abstract class WeatherDatabase: RoomDatabase(){
+    abstract fun weatherDao(): WeatherDao
 }
