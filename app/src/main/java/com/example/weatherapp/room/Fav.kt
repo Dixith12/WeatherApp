043d612d8d64d3f3dc5540fcb8a6886d077b617c
Deekshith <deekshithskulal485@gmail.com>
2025-08-14 23:09:25 +0530

    feat: add query to WeatherDao

--- app/src/main/java/com/example/weatherapp/room/Fav.kt
+++ app/src/main/java/com/example/weatherapp/room/Fav.kt
@@ -6,9 +6,13 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "Fav_table")
 data class Fav(
-
     @PrimaryKey
     @ColumnInfo(name = "city")
     val city: String,
-
+    @ColumnInfo(name= "min")
+    val min:Int,
+    @ColumnInfo(name = "max")
+    val max:Int,
+    @ColumnInfo(name = "degree")
+    val degree:Int,
 )
--- app/src/main/java/com/example/weatherapp/room/WeatherDao.kt
+++ app/src/main/java/com/example/weatherapp/room/WeatherDao.kt
@@ -1,8 +1,23 @@
 package com.example.weatherapp.room
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WeatherDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(fav: Fav)
+
+    @Query("DELETE FROM fav_table WHERE city = :city")
+    suspend fun deleteFavByCity(city: String)
+
+    @Query("SELECT EXISTS(SELECT 1 FROM fav_table WHERE city = :city LIMIT 1)")
+    fun isFavourite(city: String): Flow<Boolean>
+
+    @Query("Select * from Fav_table")
+    fun getFav(): Flow<List<Fav>>
 
 }
